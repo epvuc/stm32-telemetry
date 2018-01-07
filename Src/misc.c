bb93dbd0d5b94b5b6730dbc68cda09e049007881
eric <eric@limpoc.com>
2018-01-06 22:35:33 -0800

    update misc.c to use common eeprom addrs from config_eeprom.h

--- Src/misc.c
+++ Src/misc.c
@@ -2,12 +2,8 @@
 #include <stdio.h>
 #include "nrf24.h"
 #include "stm32l0xx_hal.h"
-
 // config settings stored in eeprom
-#define EEADDR_INT_MIN 16
-#define EEADDR_INT_SEC 17
-#define EE_INT_MIN  (*(uint8_t *)(DATA_EEPROM_BASE + EEADDR_INT_MIN))
-#define EE_INT_SEC  (*(uint8_t *)(DATA_EEPROM_BASE + EEADDR_INT_SEC))
+#include "config_eeprom.h"
 
 uint8_t unhex(char h, char l)
 {
